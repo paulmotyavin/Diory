d4c39e63df9c20e3bebba302654bb9e036174c82
paulmotyavin <isip_p.d.motyavin@mpt.ru>
2022-10-21 00:42:29 +0300

    Немного поменял, но пока что еще не работает(

--- Diary/Class1.cs
+++ Diary/Class1.cs
@@ -1,82 +1,9 @@
 ﻿namespace Diary
 {
-    public class Zametki
+    public class Zametka
     {
-        public string name;
-        public string description;
-        public DateTime DateOfZametki;
-        private void Main()
-        {
-            string name;
-            string description;
-            DateTime DateOfZametki; 
-        }
-        public string[] Zametka10()
-        {   
-            string name;
-            string description;
-            DateTime DateOfZametki;
-            name = "Сходить на пары";
-            description = "Учеба начинается со 2 пары до 5";
-            string[] bebra = { name, description };
-            return bebra;
-        }
-        public void Zametka1()
-        {
-            DateTime day = new(2022, 10, 19);
-            int position = 2;
-            day.ToString();
-            day = day.Date;
-            day.ToString();
-            Console.WriteLine(day);
-            ConsoleKeyInfo key = Console.ReadKey();
-            Zametki zametka1 = new Zametki();
-            zametka1.name = "1. Идти на пары";
-            description = "Учеба начинается со 2 пары до 5.";
-            zametka1.DateOfZametki = day;
-        }
-        public void Zametka2()
-        {
-            Zametki zametka2 = new Zametki();
-            zametka2.name = "1. Пойти в магазин";
-            description = "Пойти в пятерочку и купить: салат, овощи, фрукты для ужина.";
-            zametka2.DateOfZametki = day;
-        }
-        public void Zametka3()
-        {
-            Zametki zametka3 = new Zametki();
-            zametka3.name = "1. Ничего не делать";
-            description = "Я сделал довольно большую работу, поэтому можно отдохнуть.";
-            zametka3.DateOfZametki = day;
-        }
-        public void Zametka4()
-        {
-            Zametki zametka3 = new Zametki();
-            zametka3.name = "1. Ничего не делать";
-            description = "Я сделал довольно большую работу, поэтому можно отдохнуть.";
-            zametka3.DateOfZametki = date;
-        }
-    }
-
-
-namespace ConsolApp1
-    {
-        internal class Pizza
-        {
-            public string Name;
-            public int Diameter;
-        }
-    }
-    Pizza margarita = new Pizza();
-    margarita.Name = "Маргарита";
-margarita.Diameter = 28;
-
-Console.WriteLine(margarita.Name);
-Console.WriteLine(margarita.Diameter);
-
-Pizza havai = new Pizza();
-    havai.Diameter = "Гавайская";
-havai.
+        public string Name;
+        public string Description;
+        public DateTime date;
     }
 }
-
--- Diary/Program.cs
+++ Diary/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
-namespace Diary;
+﻿namespace Diary;
 internal class Program
 {
 
@@ -27,26 +25,63 @@ internal class Program
             Console.SetCursorPosition(0, position);
             Console.WriteLine("->");
             key = Console.ReadKey();
-            Bebra();
         }
     }
-    static void Bebra()
+    static void Main2()
     {
-        DateTime day = new(2022, 10, 19);
-        int position = 2;
-        day.ToString();
-        day = day.Date;
-        day.ToString();
-        Console.WriteLine(day);
-        ConsoleKeyInfo key = Console.ReadKey();
-        Zametki zametka1 = new Zametki
+        Zametka1();
+        foreach (Zametka pos in menuList)
         {
-            name = "1. Идти на пары",
-            description = "Учеба начинается со 2 пары до 5.",
-            DateOfZametki = day
-        };
-        Console.WriteLine(zametka1.name);
+            Console.WriteLine("Название: " + pos.Name);
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Описание: " + pos.Description);
+            Console.WriteLine("Дата: " + pos.date);
+        }
+        foreach (Zametka pos in menuList)
+        {
+            Console.WriteLine("# " + pos.Name);
+        }
     }
+    static void Zametka1()
+    {
+        Zametka number1 = new();
+        number1.Name = "Пойти поспать";
+        number1.Description = "я очень устал из-за прошлых заданий, поэтому лучше отоспаться";
+        number1.date = new(2022, 10, 19);
+        List<Zametka> menuList = new List<Zametka>();
+        menuList.Add(number1);
+    }
+    static void Zametka2()
+    {
+        Zametka number2 = new();
+        number2.Name = "Пойти погулять";
+        number2.Description = "я обещал Насте, что погуляю с ней";
+        number2.date = new(2022, 10, 20);
+        menuList.Add(number2);
+    }
+    static void Zametka3()
+    {
+        Zametka number3 = new();
+        number3.Name = "Сделать подарок маме";
+        number3.Description = "У мамы день рождения и нужно приготовить подарок своими руками.";
+        number3.date = new(2022, 10, 22);
+        menuList.Add(number3);
+    }
+    static void Zametka4()
+    {
+        Zametka number4 = new Zametka();
+        number4.Name = "Сделать домамшнее задание";
+        number4.Description = "Выучить конспект по ААС, завершить практическую по пайтону и приступить к C#.";
+        number4.date = new(2022, 10, 23);
+        menuList.Add(number4);
+    }
+    static void Zametka5()
+    {
+        Zametka number5 = new Zametka();
+        number5.Name = "Выспаться";
+        number5.Description = "Я выполнил много дел, пора отдохнуть и хорошенько выспаться";
+        number5.date = new(2022, 10, 24);
+        menuList.Add(number5);
+    }
+
 }
-    
-           
